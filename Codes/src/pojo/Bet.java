e18b35c0bce041f797a9850171e69c2be022e6ce
Rafael Gonçalves Lima <rafaellimataua@hotmail.com>
2018-11-13 11:44:53 -0300

    Relational Model changed

---
+++ Codes/src/pojo/Bet.java
@@ -0,0 +1,13 @@
+package pojo;
+
+import java.util.ArrayList;
+
+public class Shot {
+    private int idBet;
+    private int idUser;
+    private ArrayList
+
+    public Shot() {
+
+    }
+}
---
+++ Codes/src/pojo/Shot.java
@@ -0,0 +1,10 @@
+package pojo;
+
+public class Bet {
+    private int idBet;
+    private int idMatch;
+    private enum winner {
+        DRAW, HOME, AWAY;
+    }
+    
+}
